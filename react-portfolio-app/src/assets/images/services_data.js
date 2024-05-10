60100444da6a1fa0ac799f3342d9c748e8d3f361
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-10 17:14:01 +0530

    completed styling Services component

--- react-portfolio-app/src/assets/images/services_data.js
+++ react-portfolio-app/src/assets/images/services_data.js
@@ -1,34 +1,40 @@
 const Services_Data = [
-    {
-        s_no:"01",
-        s_name:"Web design",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    {
-        s_no:"02",
-        s_name:"Graphics design",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    {
-        s_no:"03",
-        s_name:"Social media",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    {
-        s_no:"04",
-        s_name:"App design",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    {
-        s_no:"05",
-        s_name:"Digital marketing",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    {
-        s_no:"06",
-        s_name:"Content writing",
-        s_desc:"Web development is the process of building, programming..."
-    },
-]
- 
+  {
+    s_no: "01",
+    s_name: "Web Design",
+    s_desc:
+      "Web designing is the process of planning, conceptualizing, and implementing the plan for designing a website in a way that is functional and offers a good user experience...",
+  },
+  {
+    s_no: "02",
+    s_name: "Graphics Design",
+    s_desc:
+      "Graphic design is a craft where professionals create visual content to communicate messages. By applying visual hierarchy and page layout techniques, designers use typography...",
+  },
+  {
+    s_no: "03",
+    s_name: "Social Media",
+    s_desc:
+      "Social media is a broad term that refers to the websites and apps that allow people to communicate, share content, and create online communities. Social media platforms include...",
+  },
+  {
+    s_no: "04",
+    s_name: "App Design",
+    s_desc:
+      "App design is the process of creating the visual and interactive elements of a mobile app, including its look and feel, layout, and flow. It combines UI/UX concepts...",
+  },
+  {
+    s_no: "05",
+    s_name: "Digital Marketing",
+    s_desc:
+      "Digital marketing, also called online marketing, is the promotion of brands to connect with potential customers using the internet and other forms of digital communication...",
+  },
+  {
+    s_no: "06",
+    s_name: "Content Writing",
+    s_desc:
+      "Content writing is the process of creating well-researched, engaging, and informative written content for organizations. Content writers create and edit content for a variety of purposes...",
+  },
+];
+
 export default Services_Data;
--- react-portfolio-app/src/components/Services/Services.jsx
+++ react-portfolio-app/src/components/Services/Services.jsx
@@ -1,7 +1,31 @@
 import "./Services.css";
+import theme_pattern from "../../assets/images/theme_pattern.svg";
+import services_data from "../../assets/images/services_data";
+import arrow_icon from "../../assets/images/arrow_icon.svg";
 
 const Services = () => {
-  return <div></div>;
+  return (
+    <div className="services">
+      <div className="services-title">
+        <h1>My Services...</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+      <div className="services-container">
+        {services_data.map((service, index) => {
+          return (
+            <div className="services-format" key={index}>
+              <h2>{service.s_name}</h2>
+              <p>{service.s_desc}</p>
+              <div className="services-read-more">
+                <p>Read More</p>
+                <img src={arrow_icon} alt="" />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Services;
