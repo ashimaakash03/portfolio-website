33a81d74c1800a56ea4db720284d9f765ddaac8d
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-10 12:47:42 +0530

    added navbar links

--- react-portfolio-app/src/components/Navbar/Navbar.jsx
+++ react-portfolio-app/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,19 @@
 import "./Navbar.css";
-
+import logo from "../../assets/images/logo.svg";
 const Navbar = () => {
-  return <div className="navbar"></div>;
+  return (
+    <div className="navbar">
+      <img src={logo} alt="" />
+      <ul className="nav-menu">
+        <li>Home</li>
+        <li>About Me</li>
+        <li>Services</li>
+        <li>Portfolio</li>
+        <li>Contact</li>
+      </ul>
+      <div className="nav-connect">Connect With Me</div>
+    </div>
+  );
 };
 
 export default Navbar;
