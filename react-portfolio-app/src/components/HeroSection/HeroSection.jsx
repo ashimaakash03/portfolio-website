cc4973dd16ced78dd57bb5c7e81ea28cd38b83d8
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-10 14:15:54 +0530

    completed styling HeroSection

--- react-portfolio-app/src/components/HeroSection/HeroSection.jsx
+++ react-portfolio-app/src/components/HeroSection/HeroSection.jsx
@@ -1,7 +1,21 @@
 import "./HeroSection.css";
+import profile_img from "../../assets/images/profile_img.svg";
 
 const HeroSection = () => {
-  return <div className="hero"></div>;
+  return (
+    <div className="hero">
+      <img src={profile_img} alt="" />
+      <h1><span>I&apos;m Alex Bennett,</span> frontend developer, based in USA.</h1>
+      <p>
+        I am a frontend developer from California, USA with 10 years of
+        experience in multiple companies like Microsoft, Apple, Tesla
+      </p>
+      <div className="hero-action">
+        <div className="hero-connect">Connect With Me</div>
+        <div className="hero-resume">Download My Resume</div>
+      </div>
+    </div>
+  );
 };
 
 export default HeroSection;
