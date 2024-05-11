dc98e5d9f55e58694e4e52577915bf52f7c6bc06
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-11 13:30:33 +0530

    completed underline effect on nav links, using state variables

--- react-portfolio-app/src/components/Navbar/Navbar.jsx
+++ react-portfolio-app/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,33 @@
 import "./Navbar.css";
 import logo from "../../assets/images/logo.svg";
+import underline from "../../assets/images/nav_underline.svg";
+import { useState } from "react";
 const Navbar = () => {
+  const [menu, setMenu] = useState("about");
   return (
     <div className="navbar">
       <img src={logo} alt="" />
       <ul className="nav-menu">
-        <li>Home</li>
-        <li>About Me</li>
-        <li>Services</li>
-        <li>Portfolio</li>
-        <li>Contact</li>
+        <li>
+          <p onClick={()=>{setMenu("home")}}>Home</p>
+          {menu === "home" ? <img src={underline} alt="" /> : <></>}
+        </li>
+        <li>
+          <p onClick={()=>{setMenu("about")}}>About Me</p>
+          {menu === "about" ? <img src={underline} alt="" /> : <></>}
+        </li>
+        <li>
+          <p onClick={()=>{setMenu("services")}}>Services</p>
+          {menu === "services" ? <img src={underline} alt="" /> : <></>}
+        </li>
+        <li>
+          <p onClick={()=>{setMenu("works")}}>Latest Works</p>
+          {menu === "works" ? <img src={underline} alt="" /> : <></>}
+        </li>
+        <li>
+          <p onClick={()=>{setMenu("contact")}}>Contact</p>
+          {menu === "contact" ? <img src={underline} alt="" /> : <></>}
+        </li>
       </ul>
       <div className="nav-connect">Connect With Me</div>
     </div>
